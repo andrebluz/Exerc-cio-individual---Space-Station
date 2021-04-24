df1e5c16ca3bcbfb6eaf896015503bf75de1adc6
andrebluz <andreluzawp@gmail.com>
2021-04-23 23:36:34 -0300

    Exercício individual - Estação Espacial
    Exercício individual - Estação Espacial

---
+++ Assets/AGENTMANAGER.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AGENTMANAGER : MonoBehaviour
+{
+    GameObject[] allAgents;
+    void Start()
+    {
+        //array contém objetos com a tag AI
+        allAgents = GameObject.FindGameObjectsWithTag("AI");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //cria variavel hit do tipo raycast
+            RaycastHit hit;
+
+            //se a conversão do posicionamento da tela para Raycast for menor de 155 unidades, retorna o valor para o raycast hit criado anteriormente
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 155))
+            {
+                //para cada objeto dentro do array da linha 7
+                foreach (GameObject a in allAgents)
+                    //setdestination para o raycast .point
+                    a.GetComponent<AICONTROL>().agent.SetDestination(hit.point);
+
+            }
+        }
+    }
+}
---
+++ Assets/AICONTROL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AICONTROL : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>(); //diz para o objeto que o agent de navmesh é ele próprio
+    }
+}
